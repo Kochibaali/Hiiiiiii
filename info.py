c34ba2affb40f34f94067e84c73c568ba81daf56
Kochibaali <144459940+Kochibaali@users.noreply.github.com>
2023-09-09 17:13:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ class evamaria(Client):
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get("API_ID", "18674011"))
 API_HASH = environ.get("API_HASH", "38d3664512757d8830601169eff5a1de")
-BOT_TOKEN = environ.get("BOT_TOKEN", "6441061113:AAFRT7jQ9nnFnHTx1JYtmUV_c5yMh-ydSVw")
+BOT_TOKEN = environ.get("BOT_TOKEN", "5491810950:AAG6Nhr-8kSCjVPDYLIKEdahA_zyTrARVXY")
 
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
@@ -77,7 +77,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://A:A@cluster0.js38lks.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Y:Y@cluster0.hxbe4jr.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 MONGO_URL = os.environ.get('MONGO_URL', "")
